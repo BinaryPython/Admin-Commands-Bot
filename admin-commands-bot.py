42a883e21e6f59a3302bdf56898fb9d46172845e
BinaryPython <Black_Reaper_Hei@yahoo.com>
2022-04-28 21:51:39 -0500

    Added names, expiration time, and check for admin on randomName command
    Added a list of semi-trolly names into the program, a check that prevents anyone without Administrator privilege's from running the command, and a time for the name change to expire (set to only five minutes for now).
    
    Hope to later add the option to pass in a "Time" argument to the command, which will allow a custom time to be passed in.

--- admin-commands-bot.py
+++ admin-commands-bot.py
@@ -2,7 +2,8 @@ import discord
 import asyncio
 from discord.ext import commands
 import logging
-# from pprint import pprint
+import random
+import datetime
 
 logging.basicConfig(level=logging.INFO)
 
@@ -29,21 +30,85 @@ async def on_disconnect():
 @bot.event
 async def on_member_update(before,after):
 	if before.id in member_penalties.keys():
-		penalty_data = member_penalties[before.id]
+		penalty_name, penalty_time, penalty_data= member_penalties[before.id]
 
-		if penalty_data[0] == "name_change" and after.nick != penalty_data[1]:
-			await after.edit(nick=penalty_data[1])
+		curTime = datetime.datetime.utcnow()
+
+		if curTime > penalty_time:
+			del member_penalties[before.id]
+			return False
+
+		if (penalty_name == "name_change" and 
+				after.nick != penalty_data):
+			await after.edit(nick=penalty_data)
 
 
 @bot.command()
-async def randomName(ctx: commands.Context, member:discord.Member):
+async def randomName(ctx: commands.Context, member:discord.Member, time=None):
 	# Give Admins the ability to randomize display names
 	# await ctx.send("Changing Name for this dude")
 
-	newName = "New Name"
-	member_penalties[member.id] = ("name_change",newName)
+	if ctx.author.guild_permissions.administrator == False:
+		await ctx.send("You don't have permissions to use this command.")
+		return False
 
+	names = [
+		"Some Nerd",
+		"Ya Boi",
+		"The Imposter",
+		"The 'Alpha' (Not Really)",
+		"Zesty Zebra",
+		"Mr.President",
+		"The Modern Rebel Girl",
+		"BoomBlaster",
+		"Goofy Girl",
+		"The Zodiac Killer",
+		"Mustard Gas",
+		"Knuckle Breaker",
+		"Osama's Ghost",
+		"Hakuna Matata",
+		"Very Sus",
+		"Greedy Scrapper",
+		"Jaded Troll",
+		"Bony Castaway",
+		"No Fun 4 You",
+		"DownInSmoke",
+		"Crash Override",
+		"Bitten by a Zombie",
+		"Fuzzy Pack",
+		"Confused Troll",
+		"Cavern Troll",
+		"A Leper",
+		"Brash Thug",
+		"Amusing Gangster",
+		"Loser of the Seven Seas",
+		"Grog Lover",
+		"Gloomy Shaman",
+		"Noxious Wretch",
+		"Mudbrew Degenerate",
+		"Reckless Assailant",
+		"Marsh Brawler",
+		"Haunting Brute",
+		"Hillforce Slugger",
+		"Defiant Wretch",
+		"Toadstool Barbarian",
+		"Defiant Vagabond",
+		"Gloomy Wretch",
+		"A Horny Simp",
+		"Blind Brawler"
+	]
+
+	#if time == None:
+	revertTime = datetime.datetime.utcnow() + datetime.timedelta(minutes=5)
+	# else:
+	# 	revertTime = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+
+	newName = random.choice(names)
+
+	member_penalties[member.id] = ("name_change",revertTime,newName)
 	await member.edit(nick=newName)
+
+	await ctx.send("The name change was completed and will last for 5 minutes")
 
 
 bot.run("#INSERT TOKEN HERE")
